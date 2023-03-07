5df57c2ebd25c283c61d9d218456b70724e8791f
ianrasshofer <122368849+ianrasshofer@users.noreply.github.com>
2023-03-07 10:36:11 +0100

    Update Assignment_chapter13-1.py

--- Course-3/Assignments/Assignment_chapter13-1.py
+++ Course-3/Assignments/Assignment_chapter13-1.py
@@ -1,13 +1,15 @@
-from urllib.request import urlopen
+rom urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
+import collections
+collections.Callable = collections.abc.Callable
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-# url: http://py4e-data.dr-chuck.net/comments_135852.html
+# url: http://py4e-data.dr-chuck.net/comments_1731045.html
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
@@ -18,5 +20,5 @@ numbers = []
 for span in spans:
     numbers.append(int(span.string))
 
-print (sum(numbers))
-# Answer is 2565
+print(sum(numbers))
+# Answer is 2664
